eaae8274eb08e6029be06e39120c3747278b0073
Anton Podviaznikov <podviaznikov@gmail.com>
2013-06-07 21:53:12 -0600

    initial implementation

--- project.clj
+++ project.clj
@@ -4,4 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [clj-http "0.7.2"]
+                 [com.cemerick/url "0.0.8"]]
   :eval-in-leiningen true)
--- src/leiningen/sitemap.clj
+++ src/leiningen/sitemap.clj
@@ -1,6 +1,21 @@
-(ns leiningen.sitemap)
+(ns leiningen.sitemap
+  "Library for updating sitemaps in Google Webmaster Tools."
+  (:require [clojure.string :as string]
+            [clj-http.client :as http]
+            [cemerick.url :as urls]))
+
+
+
+(defn update-sitemap [url]
+  (let [encoded (urls/encode-url url)
+        full-url (str "www.google.com/webmasters/tools/ping?sitemap=" encoded)
+        resp (http/get full-url)]
+    (println "result" resp)
+    resp))
 
 (defn sitemap
   "Update sitemaps in Google Webmaster Tools."
   [project & args]
-  (println "Hi!"))
+  (let [urls (-> project :sitemaps)]
+    (for [url urls]
+      (update-sitemap url))))
