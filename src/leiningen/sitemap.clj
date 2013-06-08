ad9ca21951be39b8d4f3db31f6d7ef22c33db085
Anton Podviaznikov <podviaznikov@gmail.com>
2013-06-07 22:36:00 -0600

    add more logs

--- src/leiningen/sitemap.clj
+++ src/leiningen/sitemap.clj
@@ -10,7 +10,7 @@
   (let [encoded (urls/url-encode url)
         full-url (str "http://www.google.com/webmasters/tools/ping?sitemap=" encoded)
         resp (http/get full-url)]
-    (println "result" resp url)
+    (println "result for url: " url ". Status 200 if ok"(:status resp))
     resp))
 
 (defn sitemap
