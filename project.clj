efe218bd379f7ca948b8e32c4110fedf8cca15bc
Anton Podviaznikov <podviaznikov@gmail.com>
2014-01-22 15:55:18 +0200

    fix version

--- project.clj
+++ project.clj
@@ -7,5 +7,5 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [clj-http "0.7.8"]
-                 [com.cemerick/url "1.0.0"]]
+                 [com.cemerick/url "0.1.0"]]
   :eval-in-leiningen true)
