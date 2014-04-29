d916e45c525f4f6ccbcc6f27710d5e48e877df13
Anton Podviaznikov <podviaznikov@gmail.com>
2014-04-30 00:58:01 +0200

    Update project.clj

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [clj-http "0.7.8"]
-                 [com.cemerick/url "0.1.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [clj-http "0.9.1"]
+                 [com.cemerick/url "0.1.1"]]
   :eval-in-leiningen true)
